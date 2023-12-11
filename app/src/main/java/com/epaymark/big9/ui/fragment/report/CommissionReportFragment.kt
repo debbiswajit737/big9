7a953edd52cb21a4da85a8f3e77475627ebcc737
debbiswajit737 <debbiswajit737@gmail.com>
2023-12-12 02:30:09 +0530

    load more button

--- app/src/main/java/com/epaymark/big9/ui/fragment/report/CommissionReportFragment.kt
+++ app/src/main/java/com/epaymark/big9/ui/fragment/report/CommissionReportFragment.kt
@@ -5,8 +5,10 @@ import android.app.Dialog
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.os.SystemClock
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
@@ -14,6 +16,7 @@ import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.epaymark.big9.R
 import com.epaymark.big9.adapter.reportAdapter.CommissionReportAdapter
@@ -37,19 +40,22 @@ import com.epaymark.big9.utils.table.DataEntity
 import com.google.gson.Gson
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class CommissionReportFragment : BaseFragment() {
     lateinit var binding: CommissionFragmentReportBinding
     private val viewModel: MyViewModel by activityViewModels()
-    var commissionReportAdapter: CommissionReportAdapter?=null
+    var commissionReportAdapter: CommissionReportAdapter? = null
     var commissionReportList = ArrayList<CommissionReportData>()
+    var commissionReportList2 = ArrayList<CommissionReportData>()
     private val myViewModel: MyViewModel by activityViewModels()
     private var loader: Dialog? = null
-    var startDate=""
-    var endDate=""
-    val startIndex = 0
-    val endIndex = 10
+    var startDate = ""
+    var endDate = ""
+    private var lastClickTime1: Long = 0
+    var startIndex = 0
+    var endIndex = 10
     private lateinit var recyclerView: RecyclerView
 
     private lateinit var pagingreportAdapter: PagingReportAdapter
@@ -66,7 +72,8 @@ class CommissionReportFragment : BaseFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.commission_fragment_report, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.commission_fragment_report, container, false)
         tableViewModel = ViewModelProvider(this)[TableViewModel::class.java]
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
@@ -83,25 +90,26 @@ class CommissionReportFragment : BaseFragment() {
     override fun onResume() {
         super.onResume()
         //reportList?.clear()
-        reportAdapter?.let {
-            it.items=ArrayList()
-            it.notifyDataSetChanged()
-        }
-        getAllData()
+        /* reportAdapter?.let {
+             it.items=ArrayList()
+             it.notifyDataSetChanged()
+         }*/
+        //getAllData()
     }
+
     private fun onViewClick() {
 
         binding.apply {
-          imgBack.setOnClickListener{
-              reportList?.clear()
-              findNavController().popBackStack()
-          }
-          //imgBack.back()
+            imgBack.setOnClickListener {
+                reportList?.clear()
+                findNavController().popBackStack()
+            }
+            //imgBack.back()
 
-          tvStartDate.setOnClickListener {
+            tvStartDate.setOnClickListener {
                 it.showDatePickerDialog(object : CallBack {
                     override fun getValue(s: String) {
-                        viewModel?.startDate?.value=s
+                        viewModel?.startDate?.value = s
                     }
 
                 })
@@ -110,13 +118,13 @@ class CommissionReportFragment : BaseFragment() {
             tvEndDate.setOnClickListener {
                 it.showDatePickerDialog(object : CallBack {
                     override fun getValue(s: String) {
-                        viewModel?.enddate?.value=s
+                        viewModel?.enddate?.value = s
                     }
 
                 })
             }
 
-            tvConfirm.setOnClickListener{
+            tvConfirm.setOnClickListener {
                 getAllData()
             }
 
@@ -130,26 +138,40 @@ class CommissionReportFragment : BaseFragment() {
 
         }
         viewModel?.apply {
-            startDate.value ="".currentdate()
-            enddate.value="".currentdate()
+            startDate.value = "".currentdate()
+            enddate.value = "".currentdate()
         }
+        binding.recycleViewReport.apply {
+            recyclerView = this
+            commissionReportAdapter = CommissionReportAdapter(ArrayList(), object : CallBack {
+                override fun getValue(s: String) {
+                    val bundle = Bundle()
+                    bundle.putString("jsonData", s)
+                    findNavController().navigate(
+                        R.id.action_reportFragment_to_reportDetailsFragment,
+                        bundle
+                    )
+                }
 
-
+            })
+            adapter = commissionReportAdapter
+        }
+        getAllData()
     }
 
     private fun getAllData() {
-        val (isLogin, loginResponse) =sharedPreff.getLoginData()
-        if (isLogin){
-            loginResponse?.let {loginData->
+        val (isLogin, loginResponse) = sharedPreff.getLoginData()
+        if (isLogin) {
+            loginResponse?.let { loginData ->
                 val data = mapOf(
                     "userid" to loginData.userid,
                     "startdate" to startDate,
                     "enddate" to endDate,
                 )
-                val gson= Gson()
+                val gson = Gson()
                 var jsonString = gson.toJson(data)
                 loginData.AuthToken?.let {
-                    myViewModel?.commissionReport(it,jsonString.encrypt())
+                    myViewModel?.commissionReport(it, jsonString.encrypt())
                     //     loader?.show()
                 }
             }
@@ -157,127 +179,223 @@ class CommissionReportFragment : BaseFragment() {
     }
 
 
-
     private fun observer() {
 
 
-
-        myViewModel?.commissionReportResponseLiveData?.observe(viewLifecycleOwner){
-                    when (it) {
-                        is ResponseState.Loading -> {
-                            loader?.show()
-                        }
-
-                        is ResponseState.Success -> {
-                            loader?.dismiss()
-                            //Toast.makeText(requireContext(), ""+it.data?.Description, Toast.LENGTH_SHORT).show()
-                            if(!it.data?.data.isNullOrEmpty()){
-                                it.data?.data?.let { responseData ->
-
-                                    lifecycleScope.launch {
-                                        /*if (responseData.isNotEmpty()) {
-                                            commissionReportList =responseData
-                                        }*/
-                                        for (index in responseData.indices) {
-                                            if (responseData.isNotEmpty()) {
-                                                responseData[index].apply {
-                                                    tableViewModel.insertData(DataEntity(desc="${responseData[index].opname} Commission ${responseData[index].comm}${responseData[index].type}"))
-                                                }
-                                            }
-                                        }
-                                   /* for (index in responseData.indices) {
-
-                                               // paging++
-
-                                                 if (responseData.isNotEmpty()) {
-                                                     commissionReportList=
-                                                     responseData[index].apply {
-                                                         val desc = "$opname   "
-                                                         if (index<=20) {
-                                                             commissionReportList.add(
-                                                             ReportModel(
-                                                                 "",
-                                                                 desc = desc,
-                                                                 price = comm,
-                                                                 imageInt = R.drawable.rounded_i
-                                                             )
-                                                         )
-                                                     }
-
-
-                                                     }
-                                                    // showPagingRecycleView()
-
-                                                 }
-
-                                            }*/
-                                        showrecycleView()
-
-
-
-
-                                    }
-                                }
-                            }
-
-                        }
-
-                        is ResponseState.Error -> {
-                            loader?.dismiss()
-                            handleApiError(it.isNetworkError, it.errorCode, it.errorMessage)
-                        }
-                    }
+        myViewModel?.commissionReportResponseLiveData?.observe(viewLifecycleOwner) {
+            when (it) {
+                is ResponseState.Loading -> {
+                    loader?.show()
                 }
 
+                is ResponseState.Success -> {
+                    loader?.dismiss()
+                    //Toast.makeText(requireContext(), ""+it.data?.Description, Toast.LENGTH_SHORT).show()
+                    if (!it.data?.data.isNullOrEmpty()) {
+                        it.data?.data?.let { responseData ->
+
+                            lifecycleScope.launch {
+                                /*if (responseData.isNotEmpty()) {
+                                    commissionReportList =responseData
+                                }*/
+                                for (index in responseData.indices) {
+                                    if (responseData.isNotEmpty()) {
+                                        responseData[index].apply {
+                                            commissionReportList.add(
+                                                CommissionReportData(
+                                                    responseData[index].opname,
+                                                    " Commission ${responseData[index].comm}",
+                                                    responseData[index].type
+                                                )
+                                            )
+                                            // tableViewModel.insertData(DataEntity(desc="${responseData[index].opname} Commission ${responseData[index].comm}${responseData[index].type}"))
+                                        }
+                                    }
+                                }
+                                /* for (index in responseData.indices) {
+
+                                            // paging++
+
+                                              if (responseData.isNotEmpty()) {
+                                                  commissionReportList=
+                                                  responseData[index].apply {
+                                                      val desc = "$opname   "
+                                                      if (index<=20) {
+                                                          commissionReportList.add(
+                                                          ReportModel(
+                                                              "",
+                                                              desc = desc,
+                                                              price = comm,
+                                                              imageInt = R.drawable.rounded_i
+                                                          )
+                                                      )
+                                                  }
 
 
+                                                  }
+                                                 // showPagingRecycleView()
 
+                                              }
 
+                                         }*/
+                                showrecycleView()
+                            }
+                        }
+                    }
 
+                }
 
+                is ResponseState.Error -> {
+                    loader?.dismiss()
+                    handleApiError(it.isNetworkError, it.errorCode, it.errorMessage)
+                }
+            }
+        }
 
 
     }
 
     fun showrecycleView() {
-        lifecycleScope.launch {
-            var data = tableViewModel.getDataInRange(0, 10)
-            data?.forEach{
-                Log.d("TAGp", "showrecycleView: "+it?.desc)
-            }
+        binding.btnHasdata.setOnClickListener {
+            loadMoreData()
         }
-        /*val handler = Handler(Looper.getMainLooper())
-        handler.post {
-            binding.recycleViewReport.apply {
-                //reportList.clear()
+      /*  binding.nsv?.viewTreeObserver?.addOnScrollChangedListener {
+            val scrollY = binding.nsv?.scrollY
+            val scrollViewHeight = binding.nsv?.height
+            val contentViewHeight = binding.nsv?.getChildAt(0)?.height
+            if (scrollViewHeight != null && contentViewHeight != null) {
+                // Check if the button is visible
+                val isButtonVisible = scrollY?.plus(scrollViewHeight) ?: 0 < contentViewHeight
 
-                commissionReportAdapter = CommissionReportAdapter(commissionReportList,  object : CallBack {
-                    override fun getValue(s: String) {
-                        val bundle = Bundle()
-                        bundle.putString("jsonData", s)
-                        findNavController().navigate(
-                            R.id.action_reportFragment_to_reportDetailsFragment,
-                            bundle
-                        )
-                    }
-
-                })
-                adapter=commissionReportAdapter
-
-
-
-
-
-
+                if (isButtonVisible) {
+                    loadMoreData()
+                } else {
+                    // Button is not visible
+                    // Add your code here
+                    // For example, disable the button
+                    // btnHasData?.isEnabled = false
+                }
             }
-
-
         }*/
+
+        //val handler = Handler(Looper.getMainLooper())
+        /*binding.recycleViewReport.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                when (event?.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        loadMoreData()
+                    }
+                    MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                        // If you want to cancel the delayed runnable when the touch is released or canceled
+                        handler.removeCallbacksAndMessages(null)
+                    }
+                }
+                // Return true to consume the touch event
+                return true
+            }
+        })*/
+        /* binding.nsv.setOnScrollChangeListener { _, _, scrollY, _, _ ->
+             val lastVisibleItemPosition =
+                 (recyclerView.layoutManager as LinearLayoutManager).findLastVisibleItemPosition()
+             val totalItemCount = commissionReportAdapter?.itemCount ?: 0
+
+             if (lastVisibleItemPosition == totalItemCount - 1 && !isLoading) {
+                 // Load more data when the last item is visible
+                 loadMoreData()
+             }
+         }*/
+
+        // Initial data loading
+        loadMoreData()
     }
 
+    private fun loadMoreData() {
+        /*if (SystemClock.elapsedRealtime() - lastClickTime1 < 5000) {
+            return
+        }*/
+        //loader?.show()
+        /*  if (isLoading) {
+              return
+          }
+          isLoading = true*/
+
+        // Increment the current page
+        currentPage++
+
+        // Calculate the start and end indices for the next page
+        //val startIndex = currentPage * itemsPerPage
+        //val endIndex = startIndex + itemsPerPage
+
+        // Call your function to load more data using the new indices
+        //getAllData(startIndex, endIndex)
+        activity?.let {
+
+            lifecycleScope.launch {
+                getAllData2()
+            }
+
+
+        }
+    }
+
+    private fun getAllData2() {
+        if (!(endIndex >= (commissionReportList.size - 1))) {
+            for (index in commissionReportList.indices) {
+                if (index >= startIndex && index <= endIndex) {
+                    var items = commissionReportList[index]
+                    items.apply {
+                        commissionReportList2.add(CommissionReportData(opname, comm, type))
+                    }
+
+                }
+            }
+
+            commissionReportAdapter?.items = commissionReportList2
+            commissionReportAdapter?.notifyDataSetChanged()
+
+        } else {
+            binding.btnHasdata.visibility = View.GONE
+        }
+        //delay(2000)
+        loader?.dismiss()
+        startIndex = endIndex + 1
+        endIndex += 10
+        //showrecycleView()
+    }
+    /*lifecycleScope.launch {
+        var data = tableViewModel.getDataInRange(0, 10)
+        data?.forEach{
+            Log.d("TAGp", "showrecycleView: "+it?.desc)
+        }
+    }*/
+    /*val handler = Handler(Looper.getMainLooper())
+    handler.post {
+        binding.recycleViewReport.apply {
+            //reportList.clear()
+
+            commissionReportAdapter = CommissionReportAdapter(commissionReportList,  object : CallBack {
+                override fun getValue(s: String) {
+                    val bundle = Bundle()
+                    bundle.putString("jsonData", s)
+                    findNavController().navigate(
+                        R.id.action_reportFragment_to_reportDetailsFragment,
+                        bundle
+                    )
+                }
+
+            })
+            adapter=commissionReportAdapter
 
 
 
 
+
+
+        }
+
+
+    }*/
 }
+
+
 
